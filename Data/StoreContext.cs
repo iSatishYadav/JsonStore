a370b463c5cc42bd459a454591d0aa569c8e937a
Satish Yadav <monstersatish@live.com>
2020-01-15 07:45:39 +0530

    Add project files.

---
+++ Data/StoreContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsonStore.Data
+{
+    public class StoreContext: DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
